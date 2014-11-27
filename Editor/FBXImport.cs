4d639466ace4ed20919a9a80479c5994df482f6b
samsonchen1989 <samsonchen1989@gmail.com>
2014-11-27 21:07:26 +0800

    Add FBX Importer script.
    1. Auto-set fbx import scale to what you like, not default 0.01

---
+++ Editor/FBXImport.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class FBXImport : AssetPostprocessor
+{
+    // Default is 0.01f, can change to 1f
+    const float ImportScale = 1.0f;
+
+    void OnPreprocessModel()
+    {
+        ModelImporter importer = assetImporter as ModelImporter;
+        importer.globalScale = ImportScale;
+    }
+}
